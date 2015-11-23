774a9cfc3a9c2371d86beef6fd0ec7bc2276e6c4
Jagannath Lenka <JagannathLenka@Jagannaths-MacBook-Pro.local>
2015-11-22 19:06:03 -0800

    fix

--- app/controllers/concerns/json_web_token.rb
+++ app/controllers/concerns/json_web_token.rb
@@ -4,7 +4,7 @@ module JsonWebToken
    class JsonWebToken
 
     def self.decode(token)
-      JSON.parse(Base64.decode64(token.split('.')[1]))
+      Base64.decode64(token.split('.')[1])
    end
   end
 end
