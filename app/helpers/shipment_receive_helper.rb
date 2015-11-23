f1828e1a8d30d6ae3689a7091e1aa3c787eaf99b
Jagannath Lenka <JagannathLenka@Jagannaths-MacBook-Pro.local>
2015-11-22 21:48:13 -0800

    fixed the scanning portion of case receive

--- app/helpers/shipment_receive_helper.rb
+++ app/helpers/shipment_receive_helper.rb
@@ -4,7 +4,7 @@ module ShipmentReceiveHelper
   def editable_input(receive)
       
       content_tag :div, :class => "field" do
-       concat(content_tag(:label,receive["description"] + ": "))
+        concat(content_tag(:div, content_tag(:label,receive["description"] + ": ")))
         concat(text_field_tag('value','', :class => 'medium new_value rf')) if (receive["validated"] == false)
         concat(content_tag(:label,receive["value"])) if (receive["validated"] == true)      
       end
