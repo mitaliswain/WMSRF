d4c4c6a0fc03de02dabebf83c9665d72d31817a9
Jagannath Lenka <JagannathLenka@Jagannaths-MacBook-Pro.local>
2015-11-28 20:26:36 -0800

    added a new option

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -9,7 +9,8 @@ class Menu
 
   def self.menu_list
     [{id: 1, description: 'Receive Case', path: '/shipmentreceive/new'},
-     {id: 2, description: 'Case Putaway', path: '/case_putaway/new'}]
+     {id: 2, description: 'Case Putaway', path: '/case_putaway/new'},
+     {id: 3, description: 'Move Case', path: '/case_putaway/new'}]
   end
 
 
